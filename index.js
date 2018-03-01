fd554291e73cbfdb14ab634b0b1dcb34f2c0b870
Ferdinand Prantl <prantlf@gmail.com>
2018-03-01 09:08:46 +0100

    feat: Change the HTML format to look like Koa11y reports
    BREAKING CHANGE: Change the HTML format to look like Koa11y reports.

--- index.js
+++ index.js
@@ -1,18 +1,57 @@
 'use strict'
 
 const fs = require('fs')
-const handlebars = require('handlebars')
 const path = require('path')
-const template = handlebars.compile(fs.readFileSync(
-  path.join(__dirname, 'template.hbs'), 'utf-8'))
 
 const objectValues = require('object.values')
 if (!Object.values) {
   objectValues.shim()
 }
 
+function formatIssues (issues, panelColor) {
+  return issues.map(function (issue) {
+    const extract = issue.extract.split('<').join('&lt;')
+    const position = 'line: ' + issue.line + ', column:' + issue.column
+    const entry =
+      '<div class="panel panel-' + panelColor + '">\n' +
+        '<div class="panel-heading"></div>\n' +
+        '<div class="panel-body">\n' +
+          issue.message + '<br><br>\n' +
+          '<pre><code>' + extract + '</code></pre>\n' +
+        '</div>\n' +
+        '<div class="panel-footer text-sm"><h4><small>' + position + '</small></h4></div>\n' +
+      '</div>\n'
+
+    return entry
+  }).join('') || ''
+}
+
+function formatFile (file) {
+  const errors = file.errors
+  const warnings = file.warnings
+  const notices = file.notices
+
+  const returnedErrors = formatIssues(errors, 'danger')
+  const returnedWarnings = formatIssues(warnings, 'warning')
+  const returnedNotices = formatIssues(notices, 'primary')
+
+  const buttonMarkup =
+      '<button class="btn btn-sm btn-danger">Errors <span class="badge">' + errors.length + '</span></button>' +
+      '<button class="btn btn-sm btn-warning">Warnings <span class="badge">' + warnings.length + '</span></button>' +
+      '<button class="btn btn-sm btn-primary">Notices <span class="badge">' + notices.length + '</span></button>'
+
+  const content =
+      '    <div class="row">\n' +
+      '      <a href="javascript:void(0)"><h2>' + file.name + '</h2></a>' +
+      '      <span class="buttons">' + buttonMarkup + '</span>\n' +
+      '    </div>\n' +
+      '    <div class="row">' + returnedErrors + returnedWarnings + returnedNotices + '</div>\n'
+
+  return content
+}
+
 module.exports = function (results) {
-  var files = {}
+  const files = {}
   var errorCount = 0
   var warningCount = 0
   var noticeCount = 0
@@ -52,10 +91,24 @@ module.exports = function (results) {
     })
   })
 
-  return template({
-    files: Object.values(files),
-    errorCount: errorCount,
-    warningCount: warningCount,
-    noticeCount: noticeCount
-  })
+  const template = fs.readFileSync(
+    path.join(__dirname, 'template.html'), 'utf8')
+
+  const buttonMarkup =
+      '<button class="btn btn-sm btn-danger">Errors <span class="badge">' + errorCount + '</span></button>' +
+      '<button class="btn btn-sm btn-warning">Warnings <span class="badge">' + warningCount + '</span></button>' +
+      '<button class="btn btn-sm btn-primary">Notices <span class="badge">' + noticeCount + '</span></button>'
+
+  const heading =
+      '    <div class="row summary">\n' +
+      '      <h1>HTML Validity Report</h1>' +
+      '      <span class="buttons">' + buttonMarkup + '</span>\n' +
+      '    </div>\n'
+
+  const content = Object.values(files)
+    .map(formatFile)
+    .join('\n')
+
+  return template.replace('<!-- Heading goes here -->', heading)
+    .replace('<!-- Content goes here -->', content)
 }
